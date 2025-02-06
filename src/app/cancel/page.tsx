eb738e546f0122383d6de2e107e5647a4cf3285d
RABNAWAZ-DOGAR-01 <rabnawazdogar9@gmail.com>
2025-02-06 23:43:19 +0500

    push

--- src/app/cancel/page.tsx
+++ src/app/cancel/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function CancelPage() {
     return (
-      <div className="mt-4">
+      <div className="mt-5">
         <div className=" text-center">
           <h1 className="text-3xl font-bold text-red-600">Payment Canceled</h1>
           <p className="mt-4 text-lg">It seems you canceled the payment.</p>
